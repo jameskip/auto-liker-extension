69924c76b4e386c19caf39bf3b62708e66a306c0
jkcon <github@xiixii.pw>
2019-04-02 19:54:15 -0500

    Update CSS selector

--- popup.js
+++ popup.js
@@ -4,14 +4,14 @@ function click(e) {
         if(window.location.href !== "https://tinder.com/") {
           alert("Go to tinder.com first!");
         }
-        const likeButton = document.getElementsByClassName("recsGamepad__button--like");
+        const likeButton = document.getElementsByClassName("button Lts($ls-s) Z(0) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) recsGamepad__button D(b) Bgc(#fff) Wc($transform) End(15px) Scale(1.1):h");
         setInterval(() => {
           likeButton[0].click();
           let keepSwiping = document.getElementsByClassName("button Lts($ls-s) Z(0) Whs(nw) Cur(p) Tt(u) Bdrs(100px) Px(24px) Py(0) H(54px) Mih(54px) Lh(50px) button--outline Bdw(2px) Bds(s) Trsdu($fast) Bdc($c-gray) C($c-gray) Bdc($c-base):h C($c-base):h Fw($semibold) Bdc($c-pink) Bdc($c-orange):h C(#fff)!:h Bg(t):h W(100%) D(b) C(#fff) Bg(t) Mt(24px) Mt(12px)--xs Mt(10px)--lsh");
           if (keepSwiping[0]) {
             keepSwiping[0].click()
           }
-        }, 1);
+        }, 50);
       })()`});
   window.close();
 }
