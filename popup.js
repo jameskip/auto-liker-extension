b6f7cbcecaf78ff365fe3c41d7f934561a480c90
ubuntu <sipdbg2>
2020-06-03 19:40:41 +0200

    fix backslashes

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ function click(e) {
           alert("Go to tinder.com first!");
         }
         setInterval(() => {
-	  const likeButton = document.querySelector("#content > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > div > main > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(10px\\)--s.Pos\\(r\\) > div > div.Pos\\(r\\).Py\\(16px\\).Px\\(4px\\).Px\\(8px\\)--ml.D\\(f\\).Jc\\(sb\\).Ai\\(c\\).Maw\\(375px\\)--m.Mx\\(a\\).Pe\\(n\\).Mt\\(-1px\\) > div:nth-child(4) > button");
+	  const likeButton = document.querySelector("#content > div > div.App__body.H\\\\(100\\\\%\\\\).Pos\\\\(r\\\\).Z\\\\(0\\\\) > div > div > main > div > div.recsCardboard.W\\\\(100\\\\%\\\\).Mt\\\\(a\\\\).H\\\\(100\\\\%\\\\)--s.Px\\\\(10px\\\\)--s.Pos\\\\(r\\\\) > div > div.Pos\\\\(r\\\\).Py\\\\(16px\\\\).Px\\\\(4px\\\\).Px\\\\(8px\\\\)--ml.D\\\\(f\\\\).Jc\\\\(sb\\\\).Ai\\\\(c\\\\).Maw\\\\(375px\\\\)--m.Mx\\\\(a\\\\).Pe\\\\(n\\\\).Mt\\\\(-1px\\\\) > div:nth-child(4) > button");
           likeButton.click();
         }, 50);
       })()`});
